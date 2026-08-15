001fc188d2c957f5e56de1f0d52c807123bd69c5
Andrea Gonzalez <andrea_denardi@hotmail.com>
2026-08-15 00:25:19 -0300

    Fix import statements in app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import os
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
