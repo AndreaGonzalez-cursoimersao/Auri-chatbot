f9f940d3ba2c62bbe132b8d0deff1cc578cedbba
Andrea Gonzalez <andrea_denardi@hotmail.com>
2026-08-15 00:19:20 -0300

    Refactor imports in app.py for consistency

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import os
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
